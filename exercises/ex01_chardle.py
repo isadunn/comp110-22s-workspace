227e724b8d33aa56aab7c989b2e85f2c44bb285a
Isabella Dunn <iad@live.unc.edu>
2022-01-23 21:35:16 -0500

    Progress on Excercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,37 @@
+"""EX01 - Chardle- A cutte step toward Wordle."""
+__author__ = 730313338 
+
+word: str = input("Enter a 5-character word: ")
+if len(word) != 5:
+    print("Word must countain 5 characters")
+    exit()
+single_character: str = input("Enter a single character: ")
+if len(single_character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+print("Searching for " + single_character + " in " + word)
+instances = 0
+
+if word[0] == single_character:
+    instances = instances + 1
+    print(single_character + " found at index 0") 
+if word[1] == single_character:
+    instances = instances + 1
+    print(single_character + " found at index 1")
+if word[2] == single_character:
+    instances = instances + 1
+    print(single_character + " found at index 2")
+if word[3] == single_character:
+    instances = instances + 1
+    print(single_character + " found at index 3") 
+if word[4] == single_character:
+    instances = instances + 1
+    print(single_character + " found at index 4")
+
+if instances == 0:
+    print("No insances of " + single_character + " found in " + word)
+if instances == 1:
+    print(instances) 
+    print("instance of " + single_character + " found in " + word)
+else:
+    print(str(instances) + " instances of " + single_character + " found in " + word)
---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking hte user for input."""
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name) 
